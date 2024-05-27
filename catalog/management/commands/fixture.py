1143394104c19ff4be2471c9ee8947c4430c45f8
Vadim <vdmkonshin@gmail.com>
2024-05-28 00:33:09 +0300

    add new fixture.py

---
+++ catalog/management/commands/fixture.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+
+
+class Command(BaseCommand):
+    help = 'Загружает данные из фикстуры'
+
+    def handle(self, *args, **options):
+        call_command('loaddata', 'products.json')
