49052fa85a1d1cedecf5806274ec1c32983b77ca
Vadim <vdmkonshin@gmail.com>
2024-06-03 16:06:23 +0300

    add new project

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import catalog, contacts
-
+from catalog.views import catalog, contacts, product, catalog_products
 
 app_name = CatalogConfig.name
 urlpatterns = [
 
-    path('', catalog, name='home'),
-    path('contacts/', contacts, name='contacts')
+    path('catalog/', catalog, name='home'),
+    path('contacts/', contacts, name='contacts'),
+    path('', product, name='product'),
+    path('product/<int:pk>/', catalog_products, name='catalog_products'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product, Category
 
 
 def catalog(requests):
@@ -12,3 +14,19 @@ def contacts(request):
         message = request.POST.get('message')
         print(f'{name} ({phone}): {message}')
     return render(request, 'contacts.html')
+
+
+def product(request):
+    product_list = Product.objects.all()
+    context = {
+        'object_list': product_list,
+    }
+    return render(request, 'product_list.html', context)
+
+
+def catalog_products(request, pk):
+    catalog_list = get_object_or_404(Category, pk=pk)
+    contex = {
+        'object_list': catalog_list,
+    }
+    return render(request, 'catalog_products.html', contex)
