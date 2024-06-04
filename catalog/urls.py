68642d9cc731595c2500ed42d7da634671cf46be
Vadim <vdmkonshin@gmail.com>
2024-06-05 01:21:51 +0300

    fix code

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
+
 from catalog.apps import CatalogConfig
-from catalog.views import catalog, contacts, product, catalog_products
+from catalog.views import home, contacts, products_list, products_detail
 
 app_name = CatalogConfig.name
-urlpatterns = [
 
-    path('catalog/', catalog, name='home'),
+urlpatterns = [
+    # path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('', product, name='product'),
-    path('product/<int:pk>/', catalog_products, name='catalog_products'),
+    path('', products_list, name='products_list'),
+    path('products/<int:pk>/', products_detail, name='products_detail')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 
-from catalog.models import Product, Category
+from catalog.models import Product
 
 
-def catalog(requests):
-    return render(requests, 'home.html')
+def home(request):
+    return render(request, template_name='home.html')
 
 
 def contacts(request):
@@ -12,21 +12,18 @@ def contacts(request):
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
-        print(f'{name} ({phone}): {message}')
-    return render(request, 'contacts.html')
+        print(f'{name} {phone} {message}')
+    return render(request, template_name='contacts.html')
 
 
-def product(request):
-    product_list = Product.objects.all()
-    context = {
-        'object_list': product_list,
-    }
+def products_list(request):
+    products = Product.objects.all()
+    context = {"products": products}
+
     return render(request, 'product_list.html', context)
 
 
-def catalog_products(request, pk):
-    catalog_list = get_object_or_404(Category, pk=pk)
-    contex = {
-        'object_list': catalog_list,
-    }
-    return render(request, 'catalog_products.html', contex)
+def products_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'products_detail.html', context)
