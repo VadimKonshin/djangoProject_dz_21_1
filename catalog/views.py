ba0123cac4ea7331dfeb5baa487ee2ed797a741b
Vadim <vdmkonshin@gmail.com>
2024-07-04 03:13:45 +0300

    add close for anonymous users

--- catalog/views.py
+++ catalog/views.py
@@ -40,40 +40,49 @@ class ProductCreateView(CreateView, LoginRequiredMixin):
     form_class = ProductForm
     success_url = reverse_lazy('catalog:product_list')
 
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        VersionFormset = inlineformset_factory(Product, Version, form=VersionForm, extra=1)
+        if self.request.method == "POST":
+            context_data['formset'] = VersionFormset(self.request.POST)
+        else:
+            context_data['formset'] = VersionFormset()
+        return context_data
+
     def form_valid(self, form):
-        product = form.save()
-        user = self.request.user
-        product.user = user
-        product.save()
+        formset = self.get_context_data()['formset']
+        self.object = form.save()
+        self.object.owner = self.request.user
+        self.object.save()
+        if formset.is_valid():
+            formset.instance = self.object
+            formset.save()
 
         return super().form_valid(form)
 
 
-class ProductUpdateView(UpdateView):
+class ProductUpdateView(UpdateView, LoginRequiredMixin):
     model = Product
     form_class = ProductForm
     success_url = reverse_lazy('catalog:product_list')
 
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
-        ProductFormset = inlineformset_factory(Product, Version, VersionForm, extra=1)
+        SubjectFormset = inlineformset_factory(Product, Version, form=VersionForm, extra=1)
         if self.request.method == "POST":
-            context_data['formset'] = ProductFormset(self.request.POST, instance=self.object)
+            context_data['formset'] = SubjectFormset(self.request.POST, instance=self.object)
         else:
-            context_data['formset'] = ProductFormset(instance=self.object)
-
+            context_data['formset'] = SubjectFormset(instance=self.object)
         return context_data
 
     def form_valid(self, form):
-        context_data = self.get_context_data()
-        formset = context_data['formset']
-        if form.is_valid() and formset.is_valid():
-            self.object = form.save()
-            formset.istance = self.object
+        formset = self.get_context_data()['formset']
+        self.object = form.save()
+        if formset.is_valid():
+            formset.instance = self.object
             formset.save()
-            return super().form_valid(form)
-        else:
-            return self.render_to_response(self.get_context_data(form=form, formset=formset))
+
+        return super().form_valid(form)
 
 
 class ProductDeleteView(DeleteView):
